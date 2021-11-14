22f7149eaebaf592e6f75a0c38096c011c04ae35
KingnusCodus <jrompre@gmail.com>
2021-11-14 15:41:33 -0500

    Added desc/url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chessica "0.1.0-alpha"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Utilities for assisting with chess games, position data."
+  :url "https://github.com/KingCode/chessica"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
