4b7212f2adb67fdb5a577c003168d613aadecb44
KingnusCodus <jrompre@gmail.com>
2021-11-14 21:47:39 -0500

    changed Maven group ID

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chessica "0.1.0-SNAPSHOT"
+(defproject org.clojars.kingnuscodus/chessica "0.1.0-SNAPSHOT"
   :description "Utilities for assisting with chess games, position data."
   :url "https://github.com/KingCode/chessica"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
